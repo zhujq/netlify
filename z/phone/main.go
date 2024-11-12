39ac1128c21dad82c691196b50312bdd6f165eec
zhujq <14065567@qq.com>
2024-11-12 11:10:05 +0800

    filename

--- z/phone/main.go
+++ z/phone/main.go
@@ -8,8 +8,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-//	"path"
-//	"runtime"
+
+	//	"path"
+	//	"runtime"
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -120,9 +121,9 @@ func find(phone_num string) (pr *PhoneRecord, err error) {
 	if len(phone_num) < 7 || len(phone_num) > 11 {
 		return nil, errors.New("illegal phone length")
 	}
-//	_, fulleFilename, _, _ := runtime.Caller(0)
-//	dir := path.Dir(fulleFilename)
-	content, err = ioutil.ReadFile("./phone.dat")
+	//	_, fulleFilename, _, _ := runtime.Caller(0)
+	//	dir := path.Dir(fulleFilename)
+	content, err = ioutil.ReadFile("phone.dat")
 	if err != nil {
 		log.Println(err)
 		return nil, errors.New("error reading phone.dat")
